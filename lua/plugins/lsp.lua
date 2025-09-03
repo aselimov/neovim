762c6733659e4ea4db84f7b2004aa5a82bfb5339
Alex Selimov <alex.selimov@ukg.com>
2025-09-03 11:21:58 -0400

    Fix java configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,7 @@
 return {
 	{
 		"mfussenegger/nvim-jdtls",
+		ft = "java",
 		config = function()
 			local jdtls = require("jdtls")
 
@@ -13,7 +14,7 @@ return {
 			local style_path = vim.fn.expand("~/.config/nvim/GoogleStyle.xml")
 
 			local config = {
-				cmd = { "jdtls" }, -- or path to your startup script
+				cmd = { "/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/java", "-Declipse.application=org.eclipse.jdt.ls.core.id1", "-Dosgi.bundles.defaultStartLevel=4", "-Declipse.product=org.eclipse.jdt.ls.core.product", "-Dlog.protocol=true", "-Dlog.level=ALL", "-Xms1g", "--add-modules=ALL-SYSTEM", "--add-opens", "java.base/java.util=ALL-UNNAMED", "--add-opens", "java.base/java.lang=ALL-UNNAMED", "-jar", vim.fn.glob(vim.fn.stdpath("data") .. "/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"), "-configuration", vim.fn.glob(vim.fn.stdpath("data") .. "/mason/packages/jdtls/config_mac"), "-data", vim.fn.expand("~/.cache/jdtls-workspace/") .. "/" .. root_dir:gsub(":", "_") },
 				root_dir = root_dir,
 				settings = {
 					java = {
@@ -163,6 +164,7 @@ return {
 
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
+				"jdtls",
 				"stylua",
 				"black",
 				"clang-format",
